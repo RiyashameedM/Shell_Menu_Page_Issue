a6e1efa5482ac156f605d5035507b93ff9c7b988
RiyashameedM <97871624+RiyashameedM@users.noreply.github.com>
2024-01-17 11:21:13 +0530

    Add files via upload

---
+++ FlyoutPageSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlyoutPageSample
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ FlyoutPageSample/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FlyoutPageSample
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked(System.Object sender, System.EventArgs e)
+		{
+			(sender as Button).HeightRequest += 50;
+		}
+	}
+}
